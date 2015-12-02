bee3eb7c7f1a6f2a39d8b306b5472526fe6a84b6
Erlend Viddal <viderl@obos.no>
2015-12-02 10:41:57 +0100

    testinsjekk for hooks

--- servicebuilder-core/src/test/java/no/obos/util/servicebuilder/ServiceBuilderDocumentationTest.java
+++ servicebuilder-core/src/test/java/no/obos/util/servicebuilder/ServiceBuilderDocumentationTest.java
@@ -37,6 +37,7 @@ public class ServiceBuilderDocumentationTest {
                         .appConfigFromJvmArg(false)
                         .julLoggingIntegration(true);
 
+
                 return cfg;
             }
         }
