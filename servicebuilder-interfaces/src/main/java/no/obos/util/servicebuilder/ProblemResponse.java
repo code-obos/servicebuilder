42957b3d137477e3669ba36c13a50829deafd157
Erlend Viddal <viderl@obos.no>
2017-02-12 22:02:20 +0100

    SB-22 Nye felter i problemresponse

--- servicebuilder-interfaces/src/main/java/no/obos/util/servicebuilder/ProblemResponse.java
+++ servicebuilder-interfaces/src/main/java/no/obos/util/servicebuilder/ProblemResponse.java
@@ -3,20 +3,25 @@ package no.obos.util.servicebuilder;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Map;
 
 @XmlRootElement
 @ToString
 @EqualsAndHashCode(of="incidentReferenceId")
 @ApiModel(value="ProblemResponse", description="Returneres ved feil. Ment for programmerere. Bør ikke eksponeres til brukere")
 public class ProblemResponse {
+    public String type;
     public String title;
     public String detail;
     public int status;
     public String incidentReferenceId;
+    public boolean suggestedUserMessageInDetail;
+    public Map<String, String> context;
 
     /**
      * JAXB default constructor
@@ -25,10 +30,21 @@ public class ProblemResponse {
     private ProblemResponse() {}
 
     @JsonCreator
-    public ProblemResponse(@JsonProperty("title") String title, @JsonProperty("detail") String detail, @JsonProperty("status") int status, @JsonProperty("incidentReferenceId") String incidentReferenceId) {
+    @Builder
+    public ProblemResponse(
+            @JsonProperty("title") String title,
+            @JsonProperty("detail") String detail,
+            @JsonProperty("status") int status,
+            @JsonProperty("incidentReferenceId") String incidentReferenceId,
+            @JsonProperty("suggestedUserMessageInDetail") boolean suggestedUserMessageInDetail,
+            @JsonProperty("type") String type,
+            @JsonProperty("context") Map<String, String> context) {
         this.title = title;
         this.detail = detail;
         this.status = status;
         this.incidentReferenceId = incidentReferenceId;
+        this.suggestedUserMessageInDetail = suggestedUserMessageInDetail;
+        this.type = type;
+        this.context = context;
     }
 }
---
+++ servicebuilder-interfaces/src/test/java/no/obos/util/servicebuilder/ProblemResponseParserTest.java
@@ -0,0 +1,54 @@
+package no.obos.util.servicebuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ProblemResponseParserTest {
+
+    private ProblemResponse problemResponse;
+
+    @Before
+    public void setUp() throws Exception {
+        Map<String, String> context = new HashMap<>();
+        context.put("banos", "banos_val");
+        problemResponse = new ProblemResponse("title", "detail", 1, "refId", true, "about:null", context);
+    }
+
+    @Test
+    public void serializeAndDeserializeJson() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(problemResponse);
+
+        ProblemResponse value = objectMapper.readValue(jsonString, ProblemResponse.class);
+        assertThat(value, is(equalTo(problemResponse)));
+    }
+
+    @Test
+    public void serializeAndDeserializeXML() throws JAXBException {
+        JAXBContext jc = JAXBContext.newInstance(ProblemResponse.class);
+        Marshaller marshaller = jc.createMarshaller();
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        marshaller.marshal(problemResponse, output);
+        ByteArrayInputStream input = new ByteArrayInputStream(output.toByteArray());
+
+        ProblemResponse unmarshal = (ProblemResponse) unmarshaller.unmarshal(input);
+        assertThat(unmarshal, is(equalTo(problemResponse)));
+    }
+}
