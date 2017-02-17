54da10dccc4e36a7171855c9d0a6322b20658dab
Erlend Viddal <viderl@obos.no>
2017-02-17 19:04:07 +0100

    SB-16 - Slettet gammel moro

--- servicebuilder-core/src/main/java/no/obos/util/servicebuilder/ServiceAddon.java
+++
@@ -1,7 +0,0 @@
-package no.obos.util.servicebuilder;
-
-public interface ServiceAddon {
-    void addToJerseyConfig(JerseyConfig jerseyConfig);
-
-    void addToJettyServer(JettyServer jettyServer);
-}
--- servicebuilder-core/src/main/java/no/obos/util/servicebuilder/ServiceAddonConfig.java
+++
@@ -1,9 +0,0 @@
-package no.obos.util.servicebuilder;
-
-public interface ServiceAddonConfig<Addon extends ServiceAddon> {
-    void addProperties(PropertyProvider properties);
-
-    void addContext(ServiceBuilder serviceBuilder);
-
-    Addon init();
-}
