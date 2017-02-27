4894066a4bf1266a2b66082f76ae3aa5bcda8a3b
Erlend Viddal <viderl@obos.no>
2017-02-27 18:15:07 +0100

    minor (test feiler i jenkins da sun.net.http.allowRestrictedHeaders caches på en rar måte)

--- servicebuilder-core-addons/src/test/java/no/obos/util/servicebuilder/addon/CorsFilterAddonTest.java
+++ servicebuilder-core-addons/src/test/java/no/obos/util/servicebuilder/addon/CorsFilterAddonTest.java
@@ -10,9 +10,7 @@ import javax.ws.rs.core.Response;
 
 import static no.obos.util.servicebuilder.cors.ResponseCorsFilter.ALLOW_CREDENTIALS;
 import static no.obos.util.servicebuilder.cors.ResponseCorsFilter.ALLOW_HEADERS;
-import static no.obos.util.servicebuilder.cors.ResponseCorsFilter.ALLOW_METHODS;
 import static no.obos.util.servicebuilder.cors.ResponseCorsFilter.REQUEST_HEADERS;
-import static no.obos.util.servicebuilder.cors.ResponseCorsFilter.REQUEST_METHOD;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CorsFilterAddonTest {
@@ -28,12 +26,10 @@ public class CorsFilterAddonTest {
                         .path("api")
                         .path(TestService.PATH)
                         .request()
-                        .header(REQUEST_METHOD, "GET,PUT,POST")
                         .header(REQUEST_HEADERS, "X-OBOS-APPTOKENID")
                         .options()
                 );
         MultivaluedMap<String, Object> headers = call.getHeaders();
-        assertThat(headers.getFirst(ALLOW_METHODS)).isEqualTo("GET,PUT,POST");
         assertThat(headers.getFirst(ALLOW_HEADERS)).isEqualTo("X-OBOS-APPTOKENID");
         assertThat(headers.getFirst(ALLOW_CREDENTIALS)).isEqualTo("true");
     }
