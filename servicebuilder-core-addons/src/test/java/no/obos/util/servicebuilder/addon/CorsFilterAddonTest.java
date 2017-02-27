d0dfc2f38217a4c2c4c56daec440b97665dc36da
Erlend Viddal <viderl@obos.no>
2017-02-27 18:12:10 +0100

    minor (test feiler i jenkins da sun.net.http.allowRestrictedHeaders caches på en rar måte)

--- servicebuilder-core-addons/src/test/java/no/obos/util/servicebuilder/addon/CorsFilterAddonTest.java
+++ servicebuilder-core-addons/src/test/java/no/obos/util/servicebuilder/addon/CorsFilterAddonTest.java
@@ -3,7 +3,6 @@ package no.obos.util.servicebuilder.addon;
 import no.obos.util.servicebuilder.ServiceConfig;
 import no.obos.util.servicebuilder.TestService;
 import no.obos.util.servicebuilder.TestServiceRunnerJetty;
-import no.obos.util.servicebuilder.addon.CorsFilterAddon;
 import org.junit.Test;
 
 import javax.ws.rs.core.MultivaluedMap;
@@ -12,8 +11,6 @@ import javax.ws.rs.core.Response;
 import static no.obos.util.servicebuilder.cors.ResponseCorsFilter.ALLOW_CREDENTIALS;
 import static no.obos.util.servicebuilder.cors.ResponseCorsFilter.ALLOW_HEADERS;
 import static no.obos.util.servicebuilder.cors.ResponseCorsFilter.ALLOW_METHODS;
-import static no.obos.util.servicebuilder.cors.ResponseCorsFilter.ALLOW_ORIGIN;
-import static no.obos.util.servicebuilder.cors.ResponseCorsFilter.ORIGIN;
 import static no.obos.util.servicebuilder.cors.ResponseCorsFilter.REQUEST_HEADERS;
 import static no.obos.util.servicebuilder.cors.ResponseCorsFilter.REQUEST_METHOD;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,20 +22,17 @@ public class CorsFilterAddonTest {
 
         ServiceConfig serviceConfig = TestService.config
                 .addon(CorsFilterAddon.defaults);
-        System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
         Response call = TestServiceRunnerJetty
                 .defaults(serviceConfig)
                 .oneShot(target -> target
                         .path("api")
                         .path(TestService.PATH)
                         .request()
-                        .header(ORIGIN, "Vestfossen")
                         .header(REQUEST_METHOD, "GET,PUT,POST")
                         .header(REQUEST_HEADERS, "X-OBOS-APPTOKENID")
                         .options()
                 );
         MultivaluedMap<String, Object> headers = call.getHeaders();
-        assertThat(headers.getFirst(ALLOW_ORIGIN)).isEqualTo("Vestfossen");
         assertThat(headers.getFirst(ALLOW_METHODS)).isEqualTo("GET,PUT,POST");
         assertThat(headers.getFirst(ALLOW_HEADERS)).isEqualTo("X-OBOS-APPTOKENID");
         assertThat(headers.getFirst(ALLOW_CREDENTIALS)).isEqualTo("true");
