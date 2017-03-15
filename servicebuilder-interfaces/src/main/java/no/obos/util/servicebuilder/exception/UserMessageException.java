6b35d1f81381b9600deda7050504f5e07886c8b7
lunpe2 <lunpe2@obos.no>
2017-03-15 10:08:15 +0100

    Legger inn konstruktører som defaulter til bad request i UserMessageException

--- servicebuilder-interfaces/src/main/java/no/obos/util/servicebuilder/exception/UserMessageException.java
+++ servicebuilder-interfaces/src/main/java/no/obos/util/servicebuilder/exception/UserMessageException.java
@@ -4,8 +4,18 @@ import lombok.Getter;
 
 import javax.ws.rs.WebApplicationException;
 
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+
 @Getter
 public class UserMessageException extends WebApplicationException {
+
+    public UserMessageException(String detail) {
+        super(detail, BAD_REQUEST);
+    }
+
+    public UserMessageException(String detail, Throwable cause) {
+        super(detail, cause, BAD_REQUEST);
+    }
 
     public UserMessageException(String detail, int status) {
         super(detail, status);
