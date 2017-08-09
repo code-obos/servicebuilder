0df61ca54a55a37da0f3216adeb70122e14724fc
garand <garand@obos.no>
2017-08-09 14:36:55 +0200

    FP-4781: Fjernet kode som kobler til på nytt ved feil og erstattet det med standard ActiveMQ auto reconnection konfig.

--- servicebuilder-activemq/src/main/java/no/obos/util/servicebuilder/mq/ActiveMqListener.java
+++ servicebuilder-activemq/src/main/java/no/obos/util/servicebuilder/mq/ActiveMqListener.java
@@ -59,25 +59,14 @@ public class ActiveMqListener implements MessageQueueListener {
         log.debug("Starting listener...");
         try {
             ActiveMQConnection connection = ActiveMqUtils.openConnection(user, password, url);
-            connection.setExceptionListener(jmsException -> restartListener(handler, jmsException));
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             Queue queue = session.createQueue(queueInput);
             MessageConsumer consumer = session.createConsumer(queue);
             consumer.setMessageListener(message -> handleMessage(handler, message, session));
             log.debug("Listening to {}", queueInput);
         } catch (Exception e) {
-            restartListener(handler, e);
+            log.error("Error starting listening to queue {} with exception {}", queueInput, e);
         }
-    }
-
-    private void restartListener(MessageHandler handler, Exception e) {
-        log.error("MQ listener failed, restarting in 10 seconds...", e);
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException ignored) {
-            // Ignored
-        }
-        startListener(handler);
     }
 
     private void handleMessage(MessageHandler handler, Message message, Session session) {
