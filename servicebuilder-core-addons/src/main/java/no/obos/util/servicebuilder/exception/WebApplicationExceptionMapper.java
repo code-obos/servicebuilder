95c7bcfb856b831e4a35cc051ce91a6da67db9ad
Frode Torvund <frode.torvund@obos.no>
2022-03-16 16:19:53 +0100

    [ITA-1726] log 500+ statuses with ERROR level

--- servicebuilder-core-addons/src/main/java/no/obos/util/servicebuilder/exception/WebApplicationExceptionMapper.java
+++ servicebuilder-core-addons/src/main/java/no/obos/util/servicebuilder/exception/WebApplicationExceptionMapper.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import java.util.logging.Logger;
 
 @Slf4j
 public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplicationException> {
@@ -19,11 +20,11 @@ public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplica
 
     @Override
     public Response toResponse(WebApplicationException exception) {
+        LogLevel logLevel = exception.getResponse().getStatus() >= 500 ? LogLevel.ERROR : LogLevel.WARN;
         return exceptionUtil.handle(exception, cfg -> cfg
                 .status(exception.getResponse().getStatus())
-                .logLevel(LogLevel.WARN)
+                .logLevel(logLevel)
                 .logger(log)
         );
     }
-
 }
